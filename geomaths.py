95ec6f3183a55d68e2cf720beedaa5dd1fe44dfe
satyarth.mishra@gmail.com <satyarth.mishra@gmail.com>
2014-11-02 00:04:13 +0000

    min_distance function

--- geomaths.py
+++ geomaths.py
@@ -3,17 +3,14 @@
 import sys
 import math
 
-# point = sys.argv[1]
-
-# visited = sys.argv[2:]
-
+# converts coordinates from degrees to radians
 def rad(pair):
 	a=pair[0]
 	b=pair[1]
 	rad_a = a*math.pi/180.0
 	rad_b = b*math.pi/180.0
 	return [rad_a,rad_b]
-
+# given a pair of coordinats, returns the geodesic distance between the two points
 def distance(a, b):
 	ca = math.acos(math.sin(a[0])*math.sin(b[0])+math.cos(a[0])*math.cos(b[0])*math.cos(a[1]-b[1]))
 	return ca
@@ -21,9 +18,10 @@ def distance(a, b):
 point=[0.0,270.0]
 visited=[[0.0,0.0],[90.0,90.0]]
 
-distances=[]
-for temp_point in visited:
-	distances.append(distance(rad(point),rad(temp_point)))
-print min(distances)
+def min_distance(point,visited):
+	distances=[]
+	for temp_point in visited:
+		distances.append(distance(rad(point),rad(temp_point)))
+	print min(distances)
 # print distance(rad(a),rad(b))
 # print distance(rad([a,b]))
